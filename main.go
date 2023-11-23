a051ff7855735f3eb144b2e0b88f5dece2cef5f9
seungryeol <seungryeol156@gmail.com>
2023-11-23 18:08:24 +0900

    r.Body 빔, EOF 에러 발생

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 	"github.com/urfave/negroni"
@@ -11,10 +13,73 @@ import (
 // 자동 랜더링
 var rd *render.Render
 
+type CalValue struct {
+	supplyPrice          int
+	cash                 int
+	loanAmount           int
+	interestRate         float64
+	principalAndInterest int
+}
+
+type Installment struct {
+	number int
+	//date                 string
+	principal            int
+	interest             int
+	principalAndInterest int
+	loanBalance          int
+}
+
+// 나중에 기간에 맞게 make로 바꿔서 slice 안쓰게 수정 필요
+type installmentSlice []Installment
+
 func MakeWebHandler() http.Handler {
 	mux := mux.NewRouter()
 	mux.Handle("/", http.FileServer(http.Dir("public")))
+	mux.HandleFunc("/profitLossCalculator", getCalculatorHandler).Methods(http.MethodGet)
 	return mux
+}
+
+func getCalculatorHandler(w http.ResponseWriter, r *http.Request) {
+
+	// r.Body 빔
+	fmt.Println(r.Body)
+	var calValue CalValue
+	// err : EOF 발생
+	err := json.NewDecoder(r.Body).Decode(&calValue)
+
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	list := make(installmentSlice, 400)
+	var idx = 1
+	for calValue.loanAmount > 0 {
+		// 이자 계산
+		interest := int(float64(calValue.loanAmount) * calValue.interestRate / 100)
+
+		// 원금 계산
+		principal := calValue.principalAndInterest - interest
+
+		// 대출잔액
+		loanBalance := calValue.loanAmount - principal
+		calValue.loanAmount -= loanBalance
+
+		// 날짜 계산해서 사이즈 지정 필요
+
+		list = append(list,
+			Installment{
+				number:               idx,
+				principal:            principal,
+				interest:             interest,
+				principalAndInterest: calValue.principalAndInterest,
+				loanBalance:          loanBalance,
+			})
+	}
+
+	rd.JSON(w, http.StatusOK, list)
 }
 
 func main() {
--- public/index.js
+++ public/index.js
@@ -1,7 +1,84 @@
 // use strict 엄격한 체크 모드
-(function($) {
+(function ($) {
     'use strict';
-    $(function() {
-        
+    $(function () {
+        $('.money-number').on('keyup', function () {
+            updateTextView($(this));
+        });
+
+        $('#calculatorForm').submit(function (event) {
+            event.preventDefault();
+            getVar()
+            $.ajax({
+                url: "/profitLossCalculator",
+                type: "GET",
+                data: JSON.stringify({
+                    supplyPrice: supplyPrice,
+                    cash: cash,
+                    loanAmount: loanAmount,
+                    interestRate: interestRate,
+                    principalAndInterest: principalAndInterest
+                }),
+                success: function(data) {
+                    console.log(data)
+                },
+                fail: function(jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR)
+                    console.log("API 요청 실패:", textStatus, errorThrown);
+                }
+            })
+        })
+
+        setVar()
     });
 })(jQuery);
+
+let supplyPrice = 156830000
+let cash = 62830000
+let loanAmount = 94000000
+let interestRate = 5.9
+let principalAndInterest = 557548
+
+
+function setVar(){
+    $("#supplyPrice").val(supplyPrice);
+    $("#cash").val(cash);
+    $("#loanAmount").val(loanAmount);
+    $("#interestRate").val(interestRate);
+    $("#principalAndInterest").val(principalAndInterest);
+
+    updateTextView($("#supplyPrice"));
+    updateTextView($("#cash"));
+    updateTextView($("#loanAmount"));
+    updateTextView($("#principalAndInterest"));
+}
+
+function getVar() {
+    supplyPrice = $("#supplyPrice").val();
+    cash = $("#cash").val();
+    loanAmount = $("#loanAmount").val();
+    interestRate = $("#interestRate").val();
+    principalAndInterest = $("#principalAndInterest").val();
+}
+
+// Text를 toLocaleString() 함수를 이용하여 숫자에 콤마를 추가
+function updateTextView(_obj) {
+    var num = getNumber(_obj.val());
+    if (num == 0) {
+        _obj.val('');
+    } else {
+        _obj.val(num.toLocaleString());
+    }
+}
+
+// _str 에서 숫자만 추출
+function getNumber(_str) {
+    var arr = _str.split('');
+    var out = new Array();
+    for (var cnt = 0; cnt < arr.length; cnt++) {
+        if (isNaN(arr[cnt]) == false) {
+            out.push(arr[cnt]);
+        }
+    }
+    return Number(out.join(''));
+}
